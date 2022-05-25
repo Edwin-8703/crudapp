84a43379aec89f450a00a1496afdbfc5e68300a8
Edwin obisa <92731347+Edwin-8703@users.noreply.github.com>
2022-05-25 13:50:17 +0300

    Add files via upload

---
+++ src/brood/stock/management/system/BroodStockManagementSystem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brood.stock.management.system;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class BroodStockManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Landing_page land =new Landing_page();
+        land.show();
+       
+    }
+    
+}
